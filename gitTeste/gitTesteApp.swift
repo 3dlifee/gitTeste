cdc7f76abdfbf9e5057a58df242929043816ff5c
3dlifeStudio <mjfernandez3d@gmail.com>
2021-03-23 16:12:02 +0000

    Initial Commit

---
+++ gitTeste/gitTesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTesteApp.swift
+//  gitTeste
+//
+//  Created by Mario Fernandes on 23/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct gitTesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
